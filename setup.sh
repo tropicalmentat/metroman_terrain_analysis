ae256212f4fa4eeb267b4b9117af86cf3468d8e9
tropicalmentat <gabtorres1011@gmail.com>
2020-08-21 07:41:50 +0800

    Installed qgis and created helper script to install optional dependencies to enable raster analysis tools

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!
+# install optional dependencies for qgis-3.14.0-2 in Linux Manjaro 20.0.3
+# with XFCE 4.14
+sudo pacman -S fcgi
+sudo pacman -S gpsbabel
+sudo pacman -S python-gdal
+sudo pacman -S python-jinja
+sudo pacman -S python-owslib
+sudo pacman -S python-pygments
+sudo pacman -S python-psycopg2
+sudo pacman -S python-numpy
+sudo pacman -S python-yaml
